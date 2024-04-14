7cc16fa82705c650108fb367e9d640d1c0bebe9b
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-04-14 12:49:31 +0200

    commit3

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -33,7 +33,7 @@ export async function removeContact(contactId) {
 
 export async function addContact(name, email, phone) {
   const contacts = await listContacts();
-  const newContact = { id: uuidv4(), name: name, email, phone };
+  const newContact = { id: uuidv4(), name, email, phone };
 
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
