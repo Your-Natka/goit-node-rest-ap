20b68564fde6423c1e7e700514d9aeb7feb3dd1c
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-04-14 11:07:26 +0200

    fixed9

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -17,7 +17,7 @@ export const updateContactSchema = Joi.object({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net", "ua"] },
   }),
-  phone: Joi.string().min(7).max(12),
+  phone: Joi.string(),
 })
   .min(1)
   .message("Body must have at least one field");
