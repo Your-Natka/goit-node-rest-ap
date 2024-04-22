1a07257e6748e8290a43c2c486a4350eff03582b
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-04-22 20:05:49 +0200

    fixed4

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import { globalErrorHandler } from "./controllers/errorController.js";
 import contactsRouter from "./routes/api/contactsRouter.js";
 import authRouter from "./routes/api/authRouter.js";
+import { DEV } from "./constants/constRoles.js";
 
 dotenv.config();
 
@@ -25,12 +26,16 @@ mongoose
     process.exit(1);
   });
 
-app.use(morgan("tiny"));
+process.env.EXPIRES_IN = DEV ? app.use(morgan("dev")) : app.use(morgan("tiny"));
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", authRouter);
-app.use("/api/contacts", contactsRouter);
+const pathPrefix = "/api";
+
+app.use("/users", authRouter);
+
+app.use(`${pathPrefix}/contacts`, contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -40,6 +45,8 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+app.use(globalErrorHandler);
 
 const port = +process.env.PORT;
 app.listen(port, () => {
--- constants/constRoles.js
+++ constants/constRoles.js
@@ -3,3 +3,4 @@ export const userSubscription = {
   STARTER: "starter",
   BUSINESS: "business",
 };
+export const DEV = "development";
