cb2018e686e06e6dfbe26aba7bd258a85ff6a69f
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-05-01 07:06:18 +0200

    finish

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,34 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import HttpError from "../helpers/HttpError.js";
-import { User } from "../models/user.js";
-import catchAcync from "../helpers/catchAsync.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import HttpError from '../helpers/HttpError.js';
+import { User } from '../models/user.js';
+import catchAcync from '../helpers/catchAsync.js';
+import { updateAvatarService } from '../servis/userServis.js';
 
 const { SECRET_KEY } = process.env;
 
+export const updAvatar = catchAcync(async (req, res) => {
+  const updatedUser = await updateAvatarService(req.body, req.user, req.file);
+  res.status(200).json({
+    user: updatedUser,
+  });
+});
+
 export const register = catchAcync(async (req, res) => {
-  const { email, password, subscription } = req.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    throw HttpError(409, "Email in use");
+  const { email } = req.body;
+  const checkImail = await User.findOne({ email });
+  if (checkImail) {
+    throw HttpError(409, 'Email in use');
   }
-
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
   const newUser = await User.create({
-    email,
-    password: passwordHash,
-    subscription,
+    ...req.body,
   });
 
   res.status(201).json({
     user: {
       email: newUser.email,
       subscription: newUser.subscription,
+      avatarURL: newUser.avatarURL,
     },
   });
 });
@@ -34,18 +37,18 @@ export const login = catchAcync(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, 'Email or password is wrong');
   }
 
   const passCompare = await bcrypt.compare(password, user.password);
   if (!passCompare) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, 'Email or password is wrong');
   }
 
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
   await User.findOneAndUpdate(user._id, { token });
 
   res.status(200).json({
@@ -75,11 +78,11 @@ export const logout = catchAcync(async (req, res) => {
 export const updateSubscription = catchAcync(async (req, res) => {
   const { _id } = req.user;
 
-  if (!req.body) throw HttpError(400, "missing field subscription");
+  if (!req.body) throw HttpError(400, 'missing field subscription');
   const { email, subscription } = await User.findByIdAndUpdate(_id, req.body, {
     new: true,
     runValidators: true,
   });
-  if (!email || !subscription) throw HttpError(404, "Not found");
+  if (!email || !subscription) throw HttpError(404, 'Not found');
   res.status(201).json({ email, subscription });
 });
