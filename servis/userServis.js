b4ad4059d0ea105d91b644f5e72faee59736da58
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-05-04 08:18:10 +0200

    start_node6

--- servis/userServis.js
+++ servis/userServis.js
@@ -3,7 +3,7 @@ import HttpError from '../helpers/HttpError.js';
 
 export const updateAvatarService = async (userData, user, file) => {
   if (file === null || file === undefined) {
-    throw HttpError(400);
+    throw HttpError(400, 'there will be a link to the image');
   }
   user.avatarURL = await ImageService.saveImage(
     file,
