72daa5b1def49443f1587296d60cb9089df073ab
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-06-13 16:58:28 +0200

    fin

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,16 +15,22 @@ export const signUp = catchAsync(async (req, res) => {
   });
 });
 
-export const logIn = catchAsync(async (req, res) => {
-  const user = await checkUser(req.body);
-
-  res.status(200).json({
-    token: user.token,
-    user: {
-      email: user.email,
-      subscription: user.subscription,
-    },
-  });
+export const logIn = catchAsync(async (req, res, next) => {
+  try {
+    const user = await checkUser(req.body);
+    if (user) {
+      throw HttpError(409, 'Email in use');
+    }
+    res.status(200).json({
+      token: user.token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export const logout = catchAsync(async (req, res) => {
